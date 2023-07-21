1e635a2fddbf55d53fd1f4fc47b372d7e10d3162
hemanth <hemanthtadikonda29@gmail.com>
2023-07-21 11:44:53 +0530

    upto user

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,6 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 # bind 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl restart mongod
