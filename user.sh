32c99389f2c4fb6474a0927f3b21cacd08cee281
hemanth <hemanthtadikonda29@gmail.com>
2023-07-21 18:43:25 +0530

    changes completed

--- user.sh
+++ user.sh
@@ -1,7 +1,9 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 cp user.service /etc/systemd/system/user.service
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
@@ -11,7 +13,7 @@ npm install
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 yum install mongodb-org-shell -y
 #update mongodb ip address here
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
